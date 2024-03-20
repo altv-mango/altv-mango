f41c7af64996bba00c3e9502d79aed48e4567389
Dominik Váradi <varadidominik2000@gmail.com>
2024-03-20 15:46:34 +0100

    fix: Fixed Event service typings

--- packages/client/src/decorators/events/on-webview.decorator.ts
+++ packages/client/src/decorators/events/on-webview.decorator.ts
@@ -1,11 +1,11 @@
 import { OnWebView as $OnWebView } from '@altv-mango/core';
 import type { Events as SharedEvents } from '@altv/shared';
 
-export function OnWebView<E extends keyof SharedEvents.CustomClientToWebViewEvent>(id: string | number, eventName?: E): MethodDecorator;
+export function OnWebView<E extends keyof SharedEvents.CustomWebViewToClientEvent>(id: string | number, eventName?: E): MethodDecorator;
 export function OnWebView<E extends string>(
     id: string | number,
-    eventName?: Exclude<E, keyof SharedEvents.CustomClientToWebViewEvent>,
+    eventName?: Exclude<E, keyof SharedEvents.CustomWebViewToClientEvent>,
 ): MethodDecorator;
-export function OnWebView<E extends string>(id: string | number, eventName?: Exclude<E, keyof SharedEvents.CustomClientToWebViewEvent>) {
+export function OnWebView<E extends string>(id: string | number, eventName?: Exclude<E, keyof SharedEvents.CustomWebViewToClientEvent>) {
     return $OnWebView(id, eventName);
 }
