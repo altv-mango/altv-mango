51ca32513f409c581adbadbbce5d7d7a35d67c7c
Wiwishka <shimankov63@gmail.com>
2024-05-14 22:43:33 +0300

    Alt server v1 player

--- packages/server/src/extension.d.ts
+++ packages/server/src/extension.d.ts
@@ -24,8 +24,37 @@ declare module '@altv/shared' {
 declare module '@altv/server' {
     export namespace RPC {
         export interface CustomServerRPC {}
-    }
 
+        export interface Player {
+            emitWebView<E extends keyof SharedEvents.CustomServerToPlayerEvent>(eventName: E, body?: unknown): void;
+            emitWebView<E extends string>(
+                id: string | number,
+                eventName: Exclude<E, keyof SharedEvents.CustomServerToPlayerEvent>,
+                body?: unknown,
+            ): void;
+            call<E extends keyof SharedRPC.CustomServerToPlayerRpcEvent>(eventName: E, body?: unknown): void;
+            call<E extends string>(
+                rpcName: Exclude<E, keyof SharedRPC.CustomServerToPlayerRpcEvent>,
+                body?: unknown,
+                options?: SharedRPCCallOptions,
+            ): Promise<SharedRPCResult>;
+            callWebView<E extends keyof SharedRPC.CustomServerToWebViewRPC>(
+                id: string | number,
+                rpcName: E,
+                body?: unknown,
+                options?: RPCCallOptions,
+            ): Promise<SharedRPCResult>;
+            callWebView<E extends string>(
+                id: string | number,
+                rpcName: Exclude<E, keyof SharedRPC.CustomServerToWebViewRPC>,
+                body?: unknown,
+                options?: RPCCallOptions,
+            ): Promise<SharedRPCResult>;
+        }
+    }
+}
+
+declare module 'alt-server' {
     export interface Player {
         emitWebView<E extends keyof SharedEvents.CustomServerToPlayerEvent>(eventName: E, body?: unknown): void;
         emitWebView<E extends string>(
