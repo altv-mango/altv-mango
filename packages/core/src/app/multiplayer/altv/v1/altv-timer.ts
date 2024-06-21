37ecfecf02f9477218226755a2ff9366d556dae8
Wiwishka <shimankov63@gmail.com>
2024-06-21 16:33:55 +0300

    fix interval timer

--- packages/core/src/app/multiplayer/altv/v1/altv-timer.ts
+++ packages/core/src/app/multiplayer/altv/v1/altv-timer.ts
@@ -4,7 +4,7 @@ export class AltVTimer implements MultiplayerTimer {
     public lastTick = Date.now();
     constructor(
         public id: number,
-        public interval: number,
+        public interval: number = 0,
         public once: boolean,
         public callback: Function,
         private clearTimer: Function,
